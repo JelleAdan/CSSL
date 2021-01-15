6319be769d304a6daa26c1c289bf666e3cc6f5ba
Jelle <jelle.adan@protonmail.com>
2021-01-15 15:27:23 +0100

    Added System.Text.Json NuGet package.

--- CSSL/RL/RLController.cs
+++ CSSL/RL/RLController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.IO.MemoryMappedFiles;
 using System.Text;
+using System.Text.Json;
 
 namespace CSSL.RL
 {
@@ -15,24 +16,24 @@ namespace CSSL.RL
         public RLController(RLLayer RLLayer)
         {
             this.RLLayer = RLLayer;
-            mmf = MemoryMappedFile.CreateNew("testmap", 10000, MemoryMappedFileAccess.ReadWrite);
         }
 
         public void Status()
         {
         }
 
-        //public string Reset()
-        //{
-        //    try
-        //    {
-        //        return JsonSerializer.Serialize(RLLayer.Reset());
-        //    }
-        //    catch (Exception exception)
-        //    {
-        //        throw exception;
-        //    }
-        //}
+        public string Reset()
+        {
+            try
+            {
+                mmf = MemoryMappedFile.CreateNew("testmap", 10000);
+                return JsonSerializer.Serialize(RLLayer.Reset());
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+        }
 
         //public string Act(int action)
         //{
