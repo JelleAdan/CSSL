c795d413155c34bd873c99033430f68d5b7afaf1
Patrick Deenen <Patrick.Deenen@nexperia.com>
2022-01-10 09:18:49 +0100

    Added non negative normal distribution

---
+++ CSSL/Utilities/Distributions/NonNegativeNormalDistribution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSSL.Utilities.Distributions
+{
+    public class NonNegativeNormalDistribution : Distribution
+    {
+        public NonNegativeNormalDistribution(double mean, double sigma) : base(mean, sigma * sigma)
+        {
+            distribution = new NormalDistribution(mean, sigma);
+        }
+
+        private NormalDistribution distribution;
+
+        public override double Next()
+        {
+            double sample = distribution.Next();
+            while (sample <= 0)
+            {
+                sample = distribution.Next();
+            }
+            return sample;
+        }
+    }
+}
